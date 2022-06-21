1cf171876cebf714f1de537ecddb9d82db2f561a
wtchangdm <1546333+wtchangdm@users.noreply.github.com>
2022-06-21 12:52:51 +0800

    fix 0049

--- leetcode/0049_group_anagrams/0049_group_anagrams_test.go
+++ leetcode/0049_group_anagrams/0049_group_anagrams_test.go
@@ -1,24 +1,31 @@
 package leetcode
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGroupAnagrams(t *testing.T) {
 
 	for _, v := range []struct {
 		input  []string
-		result [][]string
+		result map[int][]string
 	}{
 		{
-			input:  []string{"eat", "tea", "tan", "ate", "nat", "bat"},
-			result: [][]string{{"eat", "tea", "ate"}, {"tan", "nat"}, {"bat"}},
+			input: []string{"eat", "tea", "tan", "ate", "nat", "bat"},
+			result: map[int][]string{
+				1: {"bat"},
+				2: {"tan", "nat"},
+				3: {"eat", "tea", "ate"},
+			},
 		},
 	} {
 		answer := groupAnagrams(v.input)
 
 		for i := range answer {
+			result := v.result[len(answer[i])]
 			for j := range answer[i] {
-				if answer[i][j] != v.result[i][j] {
-					t.Errorf("error: %s != %s", answer[i][j], v.result[i][j])
+				if answer[i][j] != result[j] {
+					t.Errorf("error: %s != %s", answer[i][j], result[j])
 				}
 			}
 		}
