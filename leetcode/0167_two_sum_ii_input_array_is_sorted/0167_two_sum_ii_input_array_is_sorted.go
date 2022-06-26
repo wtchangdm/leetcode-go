b1e12be042292052d5f596f4b044f4ce416fe2c2
wtchangdm <1546333+wtchangdm@users.noreply.github.com>
2022-06-26 15:31:23 +0800

    add 167

---
+++ leetcode/0167_two_sum_ii_input_array_is_sorted/0167_two_sum_ii_input_array_is_sorted.go
@@ -0,0 +1,20 @@
+package leetcode
+
+func twoSum(numbers []int, target int) []int {
+	left, right := 0, len(numbers)-1
+
+	for left < right {
+		sum := numbers[left] + numbers[right]
+		if sum == target {
+			return []int{left + 1, right + 1}
+		}
+
+		if sum < target {
+			left++
+		} else if sum > target {
+			right--
+		}
+	}
+
+	return []int{}
+}
---
+++ leetcode/0167_two_sum_ii_input_array_is_sorted/0167_two_sum_ii_input_array_is_sorted_test.go
@@ -0,0 +1,21 @@
+package leetcode
+
+import "testing"
+
+func TestTwoSum(t *testing.T) {
+	for _, v := range []struct {
+		input          []int
+		target         int
+		expectedResult []int
+	}{
+		{input: []int{2, 7, 11, 15}, target: 9, expectedResult: []int{1, 2}},
+		{input: []int{2, 3, 4}, target: 6, expectedResult: []int{1, 3}},
+	} {
+		result := twoSum(v.input, v.target)
+		for i := range v.expectedResult {
+			if v.expectedResult[i] != result[i] {
+				t.Errorf("error: %d != %d", v.expectedResult[i], result[i])
+			}
+		}
+	}
+}
