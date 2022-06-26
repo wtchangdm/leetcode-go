02addc9f49b963c601520a104a45250a4c0604a1
wtchangdm <1546333+wtchangdm@users.noreply.github.com>
2022-06-26 16:45:04 +0800

    update 167

--- leetcode/0167_two_sum_ii_input_array_is_sorted/0167_two_sum_ii_input_array_is_sorted.go
+++ leetcode/0167_two_sum_ii_input_array_is_sorted/0167_two_sum_ii_input_array_is_sorted.go
@@ -1,5 +1,6 @@
 package leetcode
 
+// time: O(n), space: O(1)
 func twoSum(numbers []int, target int) []int {
 	left, right := 0, len(numbers)-1
 
@@ -14,6 +15,24 @@ func twoSum(numbers []int, target int) []int {
 		} else if sum > target {
 			right--
 		}
+	}
+
+	return []int{}
+}
+
+// time: O(n), space: O(n), but doesn't require the array to be sorted
+func twoSumWithMap(numbers []int, target int) []int {
+	cache := map[int]int{}
+
+	for i := range numbers {
+		complement := target - numbers[i]
+		complementIndex, found := cache[complement]
+
+		if found {
+			return []int{complementIndex + 1, i + 1}
+		}
+
+		cache[numbers[i]] = i
 	}
 
 	return []int{}
--- leetcode/0167_two_sum_ii_input_array_is_sorted/0167_two_sum_ii_input_array_is_sorted_test.go
+++ leetcode/0167_two_sum_ii_input_array_is_sorted/0167_two_sum_ii_input_array_is_sorted_test.go
@@ -19,3 +19,21 @@ func TestTwoSum(t *testing.T) {
 		}
 	}
 }
+
+func TestTwoSumWithMap(t *testing.T) {
+	for _, v := range []struct {
+		input          []int
+		target         int
+		expectedResult []int
+	}{
+		{input: []int{2, 7, 11, 15}, target: 9, expectedResult: []int{1, 2}},
+		{input: []int{2, 3, 4}, target: 6, expectedResult: []int{1, 3}},
+	} {
+		result := twoSumWithMap(v.input, v.target)
+		for i := range v.expectedResult {
+			if v.expectedResult[i] != result[i] {
+				t.Errorf("error: %d != %d", v.expectedResult[i], result[i])
+			}
+		}
+	}
+}
