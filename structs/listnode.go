630a37f52d780a38de99be5159453b5b97a998d1
wtchangdm <1546333+wtchangdm@users.noreply.github.com>
2022-07-07 01:10:13 +0800

    add BuildListNode helper

--- structs/listnode.go
+++ structs/listnode.go
@@ -5,3 +5,18 @@ type ListNode struct {
 	Val  int
 	Next *ListNode
 }
+
+func BuildListNode(seq []int) *ListNode {
+	head := &ListNode{}
+	current := head
+
+	for i := range seq {
+		current.Val = seq[i]
+		if i < len(seq)-1 {
+			current.Next = &ListNode{}
+			current = current.Next
+		}
+	}
+
+	return head
+}
